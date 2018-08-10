51dab46ac18184ff047acc1a775f7c6a85d6acdb
JaKiMo3824 <jacob.moullet@gmail.com>
2018-08-10 14:32:57 -0700

    methodsExercise4
    exercises 1-5

---
+++ methodsExercise4/Methods.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace methodsExercise4
+{
+    class Methods
+    {
+        public static int Problem1(int num1, int num2 = 4)
+        {
+            int result = (num1 + num1) + (num2 + num2);
+            return result;
+        }
+    }
+}
--- methodsExercise4/Program.cs
+++ methodsExercise4/Program.cs
@@ -10,6 +10,15 @@ namespace methodsExercise4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Please enter a number.");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter another number(not required) or just press enter.");
+            int num2 = Int32.TryParse(Console.ReadLine(), out num2) ? num2 : 4;    
+            
+            int user1 = Methods.Problem1(num1, num2);
+            Console.WriteLine(user1);
+            Console.ReadLine();
         }
     }
 }
